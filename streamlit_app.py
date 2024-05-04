d6e3475c020ecf24e42c30ca7d9c0a1dd03302a0
Anjali Gour <163406499+ANJALI-009@users.noreply.github.com>
2024-05-04 12:13:30 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from groq import Groq
+
+
+client = Groq(
+ api_key=("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"),
+)
+
+
+
+def get_chat_completion(user_message):
+    """
+    Generate a chat completion using the Groq API.
+    """
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_message,
+            }
+        ],
+        model="mixtral-8x7b-32768",
+    )
+    return chat_completion.choices[0].message.content
+
+# Streamlit app layout with CSS styles
+st.markdown(
+    """
+    <style>
+        .stApp {
+            width: 100vw;
+            height: 100vh;
+            position: absolute;
+            left: -20%;
+            top: -10%;
+            background-image:url("https://images.playground.com/1faea2b91ca0440c9c8e566ee50ef82a.jpeg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: right;
+        }
+        .stTextInput, .stButton {
+            width: auto;
+            margin-right: 10px;
+            margin-bottom: 10px;
+        }
+        .stTitle {
+            text-align: left;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+# Streamlit app layout
+st.title("Hey! Anjali here ")
+
+# User input for message
+user_message = st.text_input("How can I help you : ")
+
+# Submit button
+if st.button("Send"):
+    # Generate chat completion
+    response = get_chat_completion(user_message)
+    
+    # Display the response outside the text area
+    st.write("Chatbot: ", response)
+
+
+
+
